ea9c9d96d62c67f470daf6ac1783ca406d0abd8a
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-06-26 18:35:24 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/srv_sboot_requisicoescomheadersapikey/SrvSbootRequisicoesComHeadersApiKeyApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.srv_sboot_requisicoescomheadersapikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvSbootRequisicoesComHeadersApiKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvSbootRequisicoesComHeadersApiKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/seteideias/srv_sboot_requisicoescomheadersapikey/SrvSbootRequisicoesComHeadersApiKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.srv_sboot_requisicoescomheadersapikey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvSbootRequisicoesComHeadersApiKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
